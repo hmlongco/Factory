a04a28c550534d90e7dbe4a434ccc7cc80b0cbd8
Danil Zvyagintsev <dzvyagintsev@apple.com>
2022-08-26 20:04:36 -0400

    enable library evolution

--- Package.swift
+++ Package.swift
@@ -26,7 +26,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Factory",
-            dependencies: []),
+            dependencies: [],
+            swiftSettings: [
+                .unsafeFlags(["-enable-library-evolution"]),
+            ]),
         .testTarget(
             name: "FactoryTests",
             dependencies: ["Factory"]),
