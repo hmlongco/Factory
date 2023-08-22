30a8bca2b11340e602356f953cd15e972c207ec1
Michael Long <hmlong@gmail.com>
2023-08-22 18:45:02 -0500

    Safe key comparison

--- Sources/Factory/Factory/Key.swift
+++ Sources/Factory/Factory/Key.swift
@@ -10,47 +10,48 @@ import Foundation
 public struct FactoryKey: Hashable {
 
     public let type: Int
-    public let key: Int
+    public let key: StaticString
+    public let hash: Int
 
-    internal init(type: Any.Type, key: StaticString = #function) {
+    public init(type: Any.Type, key: StaticString) {
+        // save type address and key
         self.type = Int(bitPattern: ObjectIdentifier(type))
-        self.key = key.hasPointerRepresentation ? Int(bitPattern: key.utf8Start) : Int(key.unicodeScalar.value)
+        self.key = key
+        // build and remember hash value
+        var hasher = Hasher()
+        hasher.combine(self.type)
+        if key.hasPointerRepresentation {
+            hasher.combine(bytes: UnsafeRawBufferPointer(start: key.utf8Start, count: key.utf8CodeUnitCount))
+        } else {
+            hasher.combine(key.unicodeScalar.value)
+        }
+        self.hash = hasher.finalize()
     }
 
+    /// Fast hash based on stored value
     public func hash(into hasher: inout Hasher) {
-        hasher.combine(type)
-        hasher.combine(key)
+        hasher.combine(hash)
     }
 
+    /// Fast equate based on stored hash and key
     public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.type == rhs.type && lhs.key == rhs.key
+        // if hashes or types don't match unequal
+        guard lhs.hash == rhs.hash && lhs.type == rhs.type else {
+            return false
+        }
+        // check key string
+        if lhs.key.hasPointerRepresentation && rhs.key.hasPointerRepresentation {
+            // safe to compare key addresses, if they match equal
+            if lhs.key.utf8Start == rhs.key.utf8Start {
+                return true
+            }
+            // we're confused, punt and compare
+            return strcmp(lhs.key.utf8Start, rhs.key.utf8Start) == 0
+        } else if lhs.key.hasPointerRepresentation == false && rhs.key.hasPointerRepresentation == false {
+            return lhs.key.unicodeScalar.value == rhs.key.unicodeScalar.value
+        }
+        // if one's a scalar and one's a pointer unequal
+        return false
     }
 
 }
-
-//public struct FactoryKey: Hashable {
-//
-//#if DEBUG
-//    let type: String
-//#endif
-//
-//    let key: String
-//
-//    internal init(type: Any.Type, key: StaticString = #function) {
-//#if DEBUG
-//        self.type = String(reflecting: type)
-//        self.key = "\(key)<\(self.type)>"
-//#else
-//        self.key = "\(key)<\(String(reflecting: type))>"
-//#endif
-//    }
-//
-//    public func hash(into hasher: inout Hasher) {
-//        hasher.combine(key)
-//    }
-//
-//    public static func == (lhs: Self, rhs: Self) -> Bool {
-//        lhs.key == rhs.key
-//    }
-//
-//}
