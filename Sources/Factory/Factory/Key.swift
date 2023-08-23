7e9281656022c09a9a4359de26c005916e53f362
Michael Long <hmlong@gmail.com>
2023-08-23 09:34:51 -0500

    Safer key comparison

--- Sources/Factory/Factory/Key.swift
+++ Sources/Factory/Factory/Key.swift
@@ -9,34 +9,26 @@ import Foundation
 
 public struct FactoryKey: Hashable {
 
-    public let type: Int
+    public let type: ObjectIdentifier
     public let key: StaticString
-    public let hash: Int
 
-    public init(type: Any.Type, key: StaticString) {
-        // save type address and key
-        self.type = Int(bitPattern: ObjectIdentifier(type))
+    public init(type: Any.Type, key: StaticString = #function) {
+        self.type = ObjectIdentifier(type)
         self.key = key
-        // build and remember hash value
-        var hasher = Hasher()
-        hasher.combine(self.type)
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(type)
         if key.hasPointerRepresentation {
             hasher.combine(bytes: UnsafeRawBufferPointer(start: key.utf8Start, count: key.utf8CodeUnitCount))
         } else {
             hasher.combine(key.unicodeScalar.value)
         }
-        self.hash = hasher.finalize()
     }
 
-    /// Fast hash based on stored value
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(hash)
-    }
-
-    /// Fast equate based on stored hash and key
     public static func == (lhs: Self, rhs: Self) -> Bool {
-        // if hashes or types don't match unequal
-        guard lhs.hash == rhs.hash && lhs.type == rhs.type else {
+        // types don't match unequal
+        guard lhs.type == rhs.type else {
             return false
         }
         // check key string
@@ -45,12 +37,12 @@ public struct FactoryKey: Hashable {
             if lhs.key.utf8Start == rhs.key.utf8Start {
                 return true
             }
-            // we're confused, punt and compare
+            // not the same string, but same value?
             return strcmp(lhs.key.utf8Start, rhs.key.utf8Start) == 0
         } else if lhs.key.hasPointerRepresentation == false && rhs.key.hasPointerRepresentation == false {
             return lhs.key.unicodeScalar.value == rhs.key.unicodeScalar.value
         }
-        // if one's a scalar and one's a pointer unequal
+        // in this context if one's a scalar and one's a pointer unequal
         return false
     }
 
--- Tests/FactoryTests/FactoryComponentTests.swift
+++ Tests/FactoryTests/FactoryComponentTests.swift
@@ -2,19 +2,26 @@ import XCTest
 @testable import Factory
 
 let key1String = StaticString(stringLiteral: "s1")
+let key1StringDup = StaticString(stringLiteral: "s1")
 let key2String = StaticString(stringLiteral: "s2")
+let key3Unicode = MyStaticScalar("\u{1F600}").value
+let key4Unicode = MyStaticScalar("\u{1F601}").value
 
 final class FactoryComponentTests: XCTestCase {
 
     let key1 = FactoryKey(type: UUID.self, key: key1String)
+    let key1D = FactoryKey(type: UUID.self, key: key1StringDup)
+    let key1S = FactoryKey(type: String.self, key: key1String)
     let key2 = FactoryKey(type: UUID.self, key: key2String)
+    let key3U = FactoryKey(type: UUID.self, key: key3Unicode)
+    let key4U = FactoryKey(type: UUID.self, key: key4Unicode)
 
     override func setUp() {
         super.setUp()
         Container.shared.reset()
     }
 
-    func testScopeCache () {
+    func testScopeCache() {
         let cache = Scope.Cache()
         let scopeID = UUID()
         let strongBox = StrongBox(scopeID: scopeID, timestamp: 0, boxed: { MyService() })
@@ -49,4 +56,50 @@ final class FactoryComponentTests: XCTestCase {
 
     }
 
+    func testFactoryKey() {
+        // All should match
+        XCTAssertEqual(key1, key1)
+        XCTAssertEqual(key1S, key1S)
+        XCTAssertEqual(key1, key1D)
+        XCTAssertEqual(key1S, key1S)
+        XCTAssertEqual(key2, key2)
+        XCTAssertEqual(key3U, key3U)
+        // Dup should match
+        XCTAssertEqual(key1, key1D)
+        // All should not match
+        XCTAssertNotEqual(key1, key1S)
+        XCTAssertNotEqual(key1, key3U)
+        XCTAssertNotEqual(key1, key2)
+        XCTAssertNotEqual(key3U, key4U)
+    }
+
+    func testFactoryKeyEdgeCases() {
+        let f1 = FactoryKey(type: Int.self, key: key1String)
+        XCTAssertEqual(f1, f1)
+        let f2a = FactoryKey(type: Int.self, key: key1String)
+        let f2b = FactoryKey(type: Int.self, key: key1StringDup)
+        XCTAssertEqual(f2a, f2b)
+        let f3 = FactoryKey(type: Int.self, key: key3Unicode)
+        XCTAssertEqual(f3, f3)
+        let f3a = FactoryKey(type: Int.self, key: key3Unicode)
+        let f3b = FactoryKey(type: Int.self, key: key3Unicode)
+        XCTAssertEqual(f3a, f3b)
+        let f4a = FactoryKey(type: Int.self, key: key1String)
+        let f4b = FactoryKey(type: Int.self, key: key3Unicode)
+        XCTAssertNotEqual(f4a, f4b)
+        var hasher = Hasher()
+        let h1 = FactoryKey(type: Int.self, key: key1String)
+        h1.hash(into: &hasher)
+        let h2 = FactoryKey(type: Int.self, key: key3Unicode)
+        h2.hash(into: &hasher)
+    }
+
+}
+
+struct MyStaticScalar: ExpressibleByUnicodeScalarLiteral {
+    typealias UnicodeScalarLiteralType = StaticString
+    let value: StaticString
+    init(unicodeScalarLiteral value: StaticString) {
+        self.value = value
+    }
 }
