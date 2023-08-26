5c4558408caf7b41d23d142ed76319f4f2530f7b
Michael Long <hmlong@gmail.com>
2023-08-26 11:04:21 -0500

    Optimized key6

--- Sources/Factory/Factory/Key.swift
+++ Sources/Factory/Factory/Key.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// FactoryKey6
 public struct FactoryKey: Hashable {
 
     public let type: ObjectIdentifier
@@ -18,33 +19,20 @@ public struct FactoryKey: Hashable {
     }
 
     public func hash(into hasher: inout Hasher) {
-        hasher.combine(type)
-        if key.hasPointerRepresentation {
-            hasher.combine(bytes: UnsafeRawBufferPointer(start: key.utf8Start, count: key.utf8CodeUnitCount))
-        } else {
-            hasher.combine(key.unicodeScalar.value)
-        }
+        hasher.combine(self.type)
     }
 
     public static func == (lhs: Self, rhs: Self) -> Bool {
-        // types don't match unequal
         guard lhs.type == rhs.type else {
             return false
         }
-        // check key string
         if lhs.key.hasPointerRepresentation && rhs.key.hasPointerRepresentation {
-            // safe to compare key addresses, if they match equal
-            if lhs.key.utf8Start == rhs.key.utf8Start {
-                return true
-            }
-            // not the same string, but same value?
-            return strcmp(lhs.key.utf8Start, rhs.key.utf8Start) == 0
+            return lhs.key.utf8Start == rhs.key.utf8Start || strcmp(lhs.key.utf8Start, rhs.key.utf8Start) == 0
         } else if lhs.key.hasPointerRepresentation == false && rhs.key.hasPointerRepresentation == false {
-            // should never, ever be scalar values but just to be complete...
             return lhs.key.unicodeScalar.value == rhs.key.unicodeScalar.value
+        } else {
+            return false
         }
-        // in this context if one's a scalar and one's a pointer unequal
-        return false
     }
 
 }
