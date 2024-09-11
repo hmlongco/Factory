677d402736a8907d5fda199de6086f904095ef8f
Michael Long <hmlong@gmail.com>
2024-09-11 18:52:05 -0500

    Some changes for release candidate

--- Package.swift
+++ Package.swift
@@ -23,19 +23,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
-//    targets: [
-//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-//        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .target(
-//            name: "Factory",
-//            dependencies: [],
-//            resources: [.copy("PrivacyInfo.xcprivacy")]
-//        ),
-//        .testTarget(
-//            name: "FactoryTests",
-//            dependencies: ["Factory"]
-//        ),
-//    ]
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
