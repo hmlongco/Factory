12f89a5368fe2173ce25b6d46d989135b9d674a5
Akos Grabecz <akos.grabecz@gmail.com>
2025-04-04 16:58:21 +0200

    move ContainerTrait to its own file

---
+++ Sources/Factory/Factory/ContainerTrait.swift
@@ -0,0 +1,28 @@
+//
+//  ContainerTrait.swift
+//  Factory
+//
+//  Created by Grabecz, Akos on 2025. 04. 04..
+//
+
+import Testing
+
+struct ContainerTrait: TestTrait, SuiteTrait, TestScoping {
+    let value: Container
+
+    func provideScope(
+        for test: Test,
+        testCase: Test.Case?,
+        performing function: () async throws -> Void
+    ) async throws {
+        try await Container.$shared.withValue(value) {
+            try await function()
+        }
+    }
+}
+
+extension Trait where Self == ContainerTrait {
+    static var container: Self {
+        Self(value: Container())
+    }
+}
