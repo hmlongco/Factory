1964fb461dc0a665918c943fd826506998f9388d
Akos Grabecz <akos.grabecz@gmail.com>
2025-04-07 11:18:55 +0200

    add custom container suggestion to the docs

--- Sources/Factory/Factory/ContainerTrait.swift
+++ Sources/Factory/Factory/ContainerTrait.swift
@@ -9,6 +9,7 @@ import Testing
 
 /// ``ContainerTrait`` is a test trait that provides a scoped container for dependency injection in tests.
 /// It allows you to isolate the default ``Container`` to a test case, thus allowing you to run Swift Testing tests in parallel.
+/// If you use a custom container, you have to create your own trait that conforms to the `TestTrait` protocol.
 /// It is also possible to leverage this behavior in `XCTestCase`, by using the `@TaskLocal` provided `withValue` method. See examples in the ``ParallelXCTests`` file.
 struct ContainerTrait: TestTrait, TestScoping {
     let value: Container
