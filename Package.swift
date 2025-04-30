37eb4aae0f409e77dd9a8c54a3a2ed172ae5e5c7
Michael Long <hmlong@gmail.com>
2025-04-30 14:19:33 -0500

    Update docc dependency in package

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
