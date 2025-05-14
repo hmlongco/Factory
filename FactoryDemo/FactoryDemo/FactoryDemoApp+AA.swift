72a978658043a31a91c440225897de65fe337fdf
Michael Long <hmlong@gmail.com>
2025-05-14 11:31:22 -0500

    Fix issue in demo app tests by ensuring FactoryKit not added to test target

--- FactoryDemo/FactoryDemo/FactoryDemoApp+AA.swift
+++ FactoryDemo/FactoryDemo/FactoryDemoApp+AA.swift
@@ -30,9 +30,15 @@ final class AAContainer: SharedContainer {
     }
 }
 
-final class AAViewModel {
-    var name: String {
-        AAContainer.shared.service().name
+extension AAContainer: AutoRegistering {
+    func autoRegister() {
+        //
     }
+}
 
+final class AAViewModel {
+    @Injected(\AAContainer.service) var service
+    var name: String {
+        service.name
+    }
 }
